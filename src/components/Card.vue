a2e687b3b405020a29bcf69f6e0034464aa8daef
MindfulLearner <dimajosh1009@gmail.com>
2024-11-05 19:05:44 +0000

    working fetch get

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<script >
+export default {
+  props: {
+    products: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Card</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <img :src="product.cover_image" alt="Product Image" />
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>Price: {{ product.price }}</p>
+        <p>Likes: {{ product.likes }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,40 @@
+<!-- cardlist presi da axios a carimcamento iniziale  -->
+<script >
+import Card from './Card.vue';
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      cards: [],
+      products: [],
+    };
+  },
+  components: {
+    Card,
+  },
+  mounted() {
+    this.fetchCards();
+  },
+  methods: {
+    fetchCards() {
+      axios.get('http://192.168.1.101:9000/api/users').then((response) => {
+        this.cards = response.data;
+        this.products = response.data.products;
+      });
+    },
+  },
+};
+</script>
+
+
+<template>
+  <div>
+    <h1>CardList</h1>
+    <div v-for="card in cards" :key="card.id">
+      <Card :card="card" :products="products" />
+    </div>
+  </div>
+</template>
+
+
