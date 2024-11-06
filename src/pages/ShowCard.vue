80011b10c2c40c16250e694ed579898a9246d248
MindfulLearner <dimajosh1009@gmail.com>
2024-11-06 21:21:22 +0000

    working show card

--- src/pages/ShowCard.vue
+++ src/pages/ShowCard.vue
@@ -1,18 +1,35 @@
 <!-- qui verra mostrato la card fulll -->
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       id: this.$route.params.id,
-      page: this.$route.params.page,
+      card: {},
     };
+  },
+  mounted() {
+    this.fetchCard();
+  },
+  methods: {
+    fetchCard() {
+      axios.get(`http://192.168.1.101:9000/api/users/${this.id}`).then((response) => {
+        this.card = response.data.product;
+        console.log(this.card);
+      });
+    },
   },
 };
 </script>
 
 <template>
   <div>
-    id: {{ id }}
-    page: {{ page }}
+    <h1>ShowCard</h1>
+    <div> id: {{ id }}</div>
+    <div> name: {{ card.name }}</div>
+    <div> description: {{ card.description }}</div>
+    <div> price: {{ card.price }}</div>
+    <div> likes: {{ card.likes }}</div>
   </div>
 </template>
